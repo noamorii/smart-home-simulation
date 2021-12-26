2eaec25189ced5bec951739bef267c4941a3251f
Chiril Babaev <babaechi@fel.cvut.cz>
2021-12-26 16:01:31 +0100

    all in one

--- home/src/main/java/stuff/Device.java
+++ home/src/main/java/stuff/Device.java
@@ -1,4 +1,37 @@
 package stuff;
+import house.Room;
+import stuff.state.DeviceState;
+import stuff.state.RestingState;
 
 public abstract class Device implements UsableObject{
+
+    DeviceState state;
+    int electricityUsed = 0;
+    final private Room room;
+
+    public DeviceState getState() {
+        return state;
+    }
+
+    public void setState(DeviceState state) {
+        this.state = state;
+    }
+
+    public int getElectricityUsed() {
+        return electricityUsed;
+    }
+
+    public void setElectricityUsed(int electricityUsed) {
+        this.electricityUsed = electricityUsed;
+    }
+
+    public Device( Room room){
+        this.state = new RestingState();
+        this.room = room;
+    }
+
+    public void usingElectricity(){
+        state.usingElectricity();
+    }
+
 }
